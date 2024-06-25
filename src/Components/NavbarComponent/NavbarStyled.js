d276c93da5bcab17560d227d97cba82ac307165b
jorgemc94 <jmaciasc1994@gmail.com>
2024-06-25 20:05:42 +0200

    Changes style navbar

--- src/Components/NavbarComponent/NavbarStyled.js
+++ src/Components/NavbarComponent/NavbarStyled.js
@@ -1,67 +1,76 @@
+
 import styled from "styled-components";
 
 
-export const Nav = styled.nav `
+export const Nav = styled.div `
     width: 100%;
-    background-color: #FFF;
-    display: flex;
-    align-content: center;
-    
+    min-height: 100vh;
+    height: 100%;
+    display: grid;
+    grid-template:
+        "menu header" 0.01fr
+        "menu content" 1fr / 20% 80%;
 `
 
 export const Menu = styled.div `
-    width:20%;
+    grid-area: menu;
+    width: 100%;
+    padding: 0rem;
+    margin: 0rem;
     display: ${props => props.$disable ? 'none' : 'block'};
-    padding: 1em;
-    align-content:center;
     box-shadow: 13px 3px 40px #00000005;
+    background-color: #FFF;
 `
-export const LogoMenu = styled.div `
+
+export const MenuLogo = styled.div `
     display: flex;
-    
+    justify-content: center;
+    padding-top: 2em;
 `
 
-export const ImageMenu = styled.img `
+export const ImageMenu = styled.img`
     width: 3em;
-`
+`;
 
-export const TextMenu = styled.div `
+export const TextMenu = styled.div`
     display: flex;
     flex-direction: column;
     padding-left: 1em;
-`
+`;
 
-export const TextLogo = styled.span `
+export const TextLogo = styled.span`
     font-size: ${props => props.$title ? '1.5em' : '0.6em'};
     font-weight: ${props => props.$title ? 700 : 400};
+`;
 
-`
-
-export const ContentNavbar = styled.div `
-    width:100%;
+export const Header = styled.div `
+    grid-area: header;
+    grid-column: ${props => props.$menuOpen ? 'span 1' : 'span 2'};
     display: flex;
     justify-content: space-between;
-    align-content: center;
+    align-items: center;
+    height: 91px;
+    box-shadow: 0px 3px 10px #00000005;
+    background-color: #FFF;
 `
 
-export const Title = styled.div `
+export const TextHeader = styled.div`
     padding: 1em;
     padding-left: 2em;
-    
-`
+`;
 
-export const TitleText = styled.h1 `
+export const TitleText = styled.h1`
     color: #262626;
     font-size: 1.75em;
     font-weight: 600;
     display: inline-block;
-    padding-left: 1em
-`
+    padding-left: 1em;
+`;
 
-export const Icons = styled.div `
+export const IconsHeader = styled.div`
     display: flex;
     gap: 2em;
     justify-content: flex-end;
     padding-right: 2em;
     align-items: center;
-`
+`;
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LoginPage } from './Pages/Login/LoginPage'
 import { NavbarComponent } from './Components/NavbarComponent/NavbarComponent'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='' element={<NavbarComponent/>}>
-    
-        </Route>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/' element={<NavbarComponent/>}>
+      </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
