f0210de0c2d88c3f4f1f14e718686b7ab7819737
jorgemc94 <jmaciasc1994@gmail.com>
2024-06-27 23:15:54 +0200

    ADD kpsiComponent to DashboardPage

--- src/Components/Comment/CommentComponent.jsx
+++ src/Components/Comment/CommentComponent.jsx
@@ -1,20 +1,24 @@
+import React from 'react';
 import { CardComment, DateComment, DetailsComment, IconsComment, ImagenComment, NameComment } from "./CommentStyled";
 import { FiXCircle } from "react-icons/fi";
 import { FaRegCheckCircle } from "react-icons/fa";
+import commentsData from '../../data/comments.json';  // Importa los datos del JSON
 
 export const CommentComponent = () => {
-
-
+    const data = commentsData;  // Asigna los datos importados
 
     return (
         <>
-            {data.map((comment) => {
+            {data.map((comment) => (
                 <CardComment key={comment.id}>
-                    <p>{comment.name}</p>
+                    {/* Utiliza `comment.client.name` para acceder al nombre del cliente */}
+                    <p>{comment.client.name}</p>
                     <DetailsComment>
-                        <ImagenComment src={comment.img}/>
+                        {/* Asegúrate de que `comment.img` esté disponible en tu JSON */}
+                        <ImagenComment src={comment.img}/>  
                         <DateComment>
-                            <NameComment $name='true'>{comment.name}</NameComment>
+                            {/* Renderiza el nombre del cliente y la fecha del comentario */}
+                            <NameComment $name='true'>{comment.client.name}</NameComment>
                             <NameComment>{comment.date}</NameComment>
                         </DateComment>
                         <IconsComment>
@@ -23,7 +27,7 @@ export const CommentComponent = () => {
                         </IconsComment>
                     </DetailsComment>
                 </CardComment>
-            })}
+            ))}
         </>
     )
 }
---
+++ src/Pages/Dashboard/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import {KpisComponent} from '../../Components/Kpis/KpisComponent';
+
+export const DashboardPage = () => {
+
+
+    return (
+        <>
+            <KpisComponent/>
+        </>
+    )
+}
