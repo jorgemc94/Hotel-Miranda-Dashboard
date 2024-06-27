a9839d312907dff24405863c6b05beddc2ce08e2
jorgemc94 <jmaciasc1994@gmail.com>
2024-06-27 12:18:06 +0200

    ADD route page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import './styles.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LoginPage } from './Pages/Login/LoginPage'
 import { LayoutPage } from './Pages/Layout/LayoutPage'
+import { DashboardPage } from './Pages/Dashboard/DasboardPage'
+import { RoomsPage } from './Pages/Rooms/RoomsPage'
+import { UsersPage } from './Pages/Users/UsersPage'
+import { ContactPage } from './Pages/Contact/ContactPage'
+import { BookingsPage } from './Pages/Bookings/BookingsPage'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +17,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
       <Route path='/login' element={<LoginPage/>}/>
       <Route path='/' element={<LayoutPage/>}>
+        <Route path='/' element={<DashboardPage/>}/>
+        <Route path='/rooms' element={<RoomsPage/>}/>
+        <Route path='/users' element={<UsersPage/>}/>
+        <Route path='/contact' element={<ContactPage/>}/>
+        <Route path='/bookings' element={<BookingsPage/>}/>
       </Route>
       </Routes>
     </BrowserRouter>
