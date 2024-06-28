3c14f1c209281be40ce71a4214c010a6bd3fd372
jorgemc94 <jmaciasc1994@gmail.com>
2024-06-28 10:34:13 +0200

    delete commits

--- src/Components/Comment/CommentComponent.jsx
+++ src/Components/Comment/CommentComponent.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 import { CardComment, DateComment, DetailsComment, IconsComment, ImagenComment, NameComment } from "./CommentStyled";
 import { FiXCircle } from "react-icons/fi";
 import { FaRegCheckCircle } from "react-icons/fa";
-import commentsData from '../../data/comments.json';  // Importa los datos del JSON
+import commentsData from '../../data/comments.json';  
 
 export const CommentComponent = () => {
-    const data = commentsData;  // Asigna los datos importados
+    const data = commentsData;  
 
     return (
         <>
             {data.map((comment) => (
                 <CardComment key={comment.id}>
-                    {/* Utiliza `comment.client.name` para acceder al nombre del cliente */}
                     <p>{comment.client.name}</p>
                     <DetailsComment>
-                        {/* Asegúrate de que `comment.img` esté disponible en tu JSON */}
                         <ImagenComment src={comment.img}/>  
                         <DateComment>
-                            {/* Renderiza el nombre del cliente y la fecha del comentario */}
                             <NameComment $name='true'>{comment.client.name}</NameComment>
                             <NameComment>{comment.date}</NameComment>
                         </DateComment>
