98a094a7709e65d11b0881e379ff274c9cb264e4
jorgemc94 <jmaciasc1994@gmail.com>
2024-07-10 18:11:17 +0200

    ADD login and logout cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test login ', () => {
+  it('login', () => {
+    cy.visit('http://localhost:5173/')
+    cy.url().should('include', '/login')
+    cy.get('#email').type('jorgemc1294@gmail.com')
+    cy.get('#password').type('12345')
+    cy.get('button').click()
+    cy.url().should('equal', 'http://localhost:5173/')
+  })
+
+  it('logout', () => {
+    cy.visit('http://localhost:5173/')
+    cy.url().should('include', '/login')
+    cy.get('#email').type('a@gmail.com')
+    cy.get('#password').type('12345')
+    cy.get('button').click()
+    cy.url().should('include', '/login')
   })
 })
+
