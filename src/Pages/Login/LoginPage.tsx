04753ff8bacb5e54bb6fbdf60befa11c5459fcce
jorgemc94 <jmaciasc1994@gmail.com>
2024-07-19 09:06:44 +0200

    add login and userContext to ts

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,19 +1,23 @@
-import { useContext, useState } from "react";
-import { LoginForm, TitleForm, Form, Label, Input, ButtonForm, LogoForm, AccessForm } from "./LoginStyled";
+import React, { useContext, useState, FormEvent } from "react";
+import { LoginForm, TitleForm, Form, Label, Input, ButtonForm, LogoForm, AccessForm, TextError} from "./LoginStyled";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/userContext";
 
-export const LoginPage = () => {
-    const { dispatch } = useContext(UserContext);
-    const [error, setError] = useState('');
+export const LoginPage: React.FC = () => {
+    const userContext = useContext(UserContext);
+    if (!userContext) {
+        throw new Error("UserContext must be used within a UserContextProvider");
+    }
+    const { dispatch } = userContext;
+    const [error, setError] = useState<string>('');
     const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const email = event.target.elements.email.value;
-        const password = event.target.elements.password.value;
+        const email = (event.target as HTMLFormElement).elements.namedItem('email') as HTMLInputElement;
+        const password = (event.target as HTMLFormElement).elements.namedItem('password') as HTMLInputElement;
 
-        if (email === 'jorgemc1294@gmail.com' && password === '12345') {
+        if (email.value === 'jorgemc1294@gmail.com' && password.value === '12345') {
             dispatch({ type: 'LOGIN', payload: { name: 'Jorge', email: 'jorgemc1294@gmail.com' } });
             navigate('/');
         } else {
@@ -26,7 +30,7 @@ export const LoginPage = () => {
             <LogoForm src="src/assets/icon.png" alt="logo" />
             <TitleForm>Login</TitleForm>
             <AccessForm>Email: jorgemc1294@gmail.com Password: 12345</AccessForm>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
+            {error && <TextError>{error}</TextError>}
             <Form onSubmit={handleSubmit}>
                 <Label>Email</Label>
                 <Input type="text" name="email" id="email" placeholder="jorgemc1294@gmail.com"></Input>
@@ -37,4 +41,3 @@ export const LoginPage = () => {
         </LoginForm>
     );
 }
-
